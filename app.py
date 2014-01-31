2531a415309ce53a226dcc243f0a9dd4c0a0828b
wodim <neikokz@gmail.com>
2014-01-31 13:15:56 +0100

    clear whitespace in templates

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = _cfg('database', 'uri')
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['PROFILE'] = True
 
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
 app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions = [30])
 
 db.app = app
